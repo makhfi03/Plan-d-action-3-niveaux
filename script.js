3a010d18129ad628d2b822757472809044315f48
makhfi03 <ghita.makhfi.21@gmail.com>
2025-12-10 22:15:43 +0100

    Exercice 1

---
+++ script.js
@@ -0,0 +1,16 @@
+//Partie Pratique — Niveau Débutant
+// Exercice 1 : Variables & types (débutant)
+
+// let nom = "ghita"
+// let age = 22
+// let estEtudiant = true
+// let note1 = 17.50
+// let note2 = 13
+
+// console.log(typeof nom);
+// console.log(typeof age);
+// console.log(typeof estEtudiant);
+// console.log(typeof note1);
+// console.log(typeof note2);
+
+
